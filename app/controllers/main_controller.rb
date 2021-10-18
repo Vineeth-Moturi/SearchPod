ebd13a74036dcf7cfaead42a201eb289e2c52bc1
Vineeth <godwillmakeaway647@gmail.com>
2021-10-18 17:27:16 +0530

    Commit on Mon 18-Oct-2021 5:26pm (Completed 80% of login& Signup Coding)

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,6 +5,38 @@ class MainController < ApplicationController
   def signup
   end
 
+  def new
+    if params != nil
+      (@users = User.new(username:params[:username],password:params[:password],email:params[:email])) if(params[:product_link]!=nil)
+    end
+    if @users.save
+      @user = User.find_by(params[:username])
+      render :new #have to render Dashboard
+      #here we calling main.html.erb from new(i.e after storing data)
+      #so initialised new @products object and called to pass instance
+    else
+    end
+  end
+
   def login
   end
+
+  def login_validate
+    @login_status = false
+    if params != nil
+      if((params[:username]!=nil) && (params[:password]!=nil) && (params[:email]!=nil))
+        @users = User.where(username:params[:username],email:params[:email]))
+        @users.each do |t|
+          if ((t.username==params[:username]) && (t.password==params[:password]) && (t.email==params[:email]))
+            @login_status = true
+          end
+        end
+      end
+    end
+    if @login_status ==true
+      render :"Login Successful"
+    else
+      render :"Login Failed..Try again"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'main/home'
   get 'main/signup'
   get 'main/login'
+  post 'main/signup', to: 'main#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
